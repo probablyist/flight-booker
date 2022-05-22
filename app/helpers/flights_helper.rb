3665e7940c421e9464fbddf7987de690b0fccc53
Paul <paul.probst@gmail.com>
2022-05-21 22:34:15 -0400

    Add helpers and formatting to flight search table

--- app/helpers/flights_helper.rb
+++ app/helpers/flights_helper.rb
@@ -2,4 +2,20 @@ module FlightsHelper
   def show_selected_airport(location_id)
     Airport.find(location_id).city
   end
+
+  def short_time_format(flight)
+    flight.departure_time.strftime('%l:%M %p')
+  end
+
+  def short_date_format(flight)
+    flight.departure_time.to_date.strftime('%Y-%m-%d')
+  end
+
+  def duration_formatted(flight)
+    duration = flight.duration
+
+    hours = duration / 60
+    minutes = duration % 60
+    "#{hours}hr #{minutes}min"
+  end
 end
